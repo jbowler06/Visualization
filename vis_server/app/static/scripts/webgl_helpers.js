4c320bd2cc28b988c9f157ebb4a6e2ef57cc319e
jack <jcb2238@columbia.edu>
2015-02-20 19:13:20 -0500

    added visualization web app to repo

---
+++ vis_server/app/static/scripts/webgl_helpers.js
@@ -0,0 +1,129 @@
+function initGL(canvas) {
+    var gl
+    try {
+        gl = canvas.getContext("webgl",{preserveDrawingBuffer: true})
+        gl.viewportWidth = canvas.width
+        gl.viewportHeight = canvas.height
+        return gl
+    } catch (e) {
+    }
+
+    if (!gl) {
+        alert("unable to initialize WebGL")
+    }
+    
+    return null
+}
+
+
+function getShader(gl, id) {
+    var shaderScript = document.getElementById(id)
+    if (!shaderScript) {
+        return null
+    }
+
+    var str = ""
+    var k = shaderScript.firstChild
+    while(k) {
+        if (k.nodeType == 3) {
+            str += k.textContent
+        }
+        k = k.nextSibling
+    }
+
+    var shader
+    if (shaderScript.type == "x-shader/x-fragment") {
+        shader = gl.createShader(gl.FRAGMENT_SHADER)
+    } else if (shaderScript.type == "x-shader/x-vertex") {
+        shader = gl.createShader(gl.VERTEX_SHADER)
+    } else {
+        return null
+    }
+    gl.shaderSource(shader, str)
+    gl.compileShader(shader)
+
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+        alert(gl.getShaderInfoLog(shader))
+        return null
+    }
+
+    return shader
+}
+
+
+function updateTextureData(texture) {
+    gl.bindTexture(gl.TEXTURE_2D, texture)
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true)
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST)
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST)
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE)
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE)
+    //gl.texImage2D(gl.TEXTURE_2D, 0, gl.LUMINANCE, data.width, data.height-2, 0, gl.LUMINANCE, gl.UNSIGNED_BYTE, new Uint8Array(data.texture) )
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.LUMINANCE, gl.LUMINANCE, gl.UNSIGNED_BYTE, texture.image )
+    gl.bindTexture(gl.TEXTURE_2D, null)
+}
+
+
+function squarePositionVerticies(size_x, size_y, z) {
+    if (!z) {
+        z = 0.0
+    }
+    size_x /= 2
+    size_y /= 2
+    var vertices = [
+         size_x,  size_y,  z,
+        -size_x,  size_y,  z,
+         size_x, -size_y,  z,
+        -size_x, -size_y,  z
+    ]
+    return vertices
+}
+
+
+function createSquareVertexPositionBuffer(gl, size_x, size_y) {
+    var squareVertexPositionBuffer = gl.createBuffer()
+    gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexPositionBuffer)
+    var vertices = squarePositionVerticies(size_x, size_y)
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW)
+    squareVertexPositionBuffer.itemSize = 3
+    squareVertexPositionBuffer.numItems = 4
+
+    return squareVertexPositionBuffer
+}
+
+
+function textureVerticies() {
+    return textureCoords = [
+            1.0, 1.0,
+            0.0, 1.0,
+            1.0, 0.0,
+            0.0, 0.0
+        ]
+}
+
+function createTextureCoordBuffer() {
+    var textureCoordBuffer = gl.createBuffer()
+    gl.bindBuffer(gl.ARRAY_BUFFER, textureCoordBuffer)
+    var textureCoords = textureVerticies()
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(textureCoords), gl.STATIC_DRAW)
+    textureCoordBuffer.itemSize = 2
+    textureCoordBuffer.numItems = 4
+    
+    return textureCoordBuffer
+}
+
+function updateTextureRGBA(texture) {
+    alert('function called - not implemented')
+    var data = new Uint8Array([100, 0, 150, 200,100,0,0,255])
+    gl.bindTexture(gl.TEXTURE_2D, texture)
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true)
+    //gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, texture.image)
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.LUMINANCE, 4, 2, 0, gl.LUMINANCE, gl.UNSIGNED_BYTE,data )
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST)
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST)
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE)
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE)
+    gl.bindTexture(gl.TEXTURE_2D, null)
+}
+
+
---
+++ vis_server/app/views.py
@@ -0,0 +1,133 @@
+from app import app
+from app.bin import dbManager
+import config
+
+from sys import path
+import numpy as np
+import traceback
+path.insert(0,config.analysis)
+path.insert(0,config.analysis+'/automaticScripts')
+
+import os.path
+import itertools as it
+import glob
+import cPickle as pickle
+import base64
+import urllib
+
+from flask import render_template
+from flask import request
+from flask import jsonify
+from flask import url_for
+from flask import send_file
+from flask import send_from_directory
+from flask import make_response
+
+from .decorators import async
+from sima import ImagingDataset
+from sima import Sequence
+
+from PIL import Image
+import StringIO
+import struct
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+@app.route('/getFrame/<frame_number>', methods=['GET','POST'])
+def getFrame(frame_number):
+    ds_path = '/data/Nathan/2photon/rewardRemapping/nd125/02112015/day1-session-003/day1-session-003.sima'
+    NORMING_VAL = 2194/255
+    channel = 0
+    ds = ImagingDataset.load(ds_path)
+    seq = ds.__iter__().next()
+    vol = seq._get_frame(int(frame_number))
+    vol /= NORMING_VAL
+    vol = np.clip(vol, 0, 255)
+
+    surf = np.nanmean(vol,axis=0)[:,:,channel]
+    img = Image.fromarray(surf.astype('uint8'),'L')
+    img_io_z = StringIO.StringIO()
+    img.save(img_io_z, 'jpeg', quality=40)
+    img_io_z.seek(0)
+
+    surf = np.nanmean(vol,axis=1)[:,:,channel]
+    img = Image.fromarray(surf.astype('uint8'),'L')
+    img_io_y = StringIO.StringIO()
+    img.save(img_io_y, 'jpeg', quality=40)
+    img_io_y.seek(0)
+
+    surf = np.fliplr(np.nanmean(vol,axis=2)[:,:,channel].T)
+    img = Image.fromarray(surf.astype('uint8'),'L')
+    img_io_x = StringIO.StringIO()
+    img.save(img_io_x, 'jpeg', quality=40)
+    img_io_x.seek(0)
+
+    return jsonify(
+        z_projection=base64.b64encode(img_io_z.read()),
+        y_projection=base64.b64encode(img_io_y.read()),
+        x_projection=base64.b64encode(img_io_x.read()))
+
+@app.route('/getFrames', methods=['GET','POST'])
+def getFrames():
+    ds_path = request.form.get('path')
+    start_frame = request.form.get('startFrame')
+    NORMING_VAL = 2194/255
+    channel = 0
+    if (os.path.splitext(ds_path)[-1] == '.sima'):
+        ds = ImagingDataset.load(ds_path)
+        seq = ds.__iter__().next()
+    else:
+        seq = Sequence.create('HDF5',ds_path,'tzyxc')
+    num_frames = 10
+    frames = {}
+    start_frame = int(start_frame)
+
+    for frame_number in xrange(start_frame,start_frame+num_frames):
+        vol = seq._get_frame(frame_number)
+        vol /= NORMING_VAL
+        vol = np.clip(vol, 0, 255)
+        surf = np.nanmean(vol,axis=0)[:,:,channel]
+        img = Image.fromarray(surf.astype('uint8'),'L')
+        img_io_z = StringIO.StringIO()
+        img.save(img_io_z, 'jpeg', quality=40)
+        img_io_z.seek(0)
+
+        surf = np.nanmean(vol,axis=1)[:,:,channel]
+        img = Image.fromarray(surf.astype('uint8'),'L')
+        img_io_y = StringIO.StringIO()
+        img.save(img_io_y, 'jpeg', quality=40)
+        img_io_y.seek(0)
+
+        surf = np.fliplr(np.nanmean(vol,axis=2)[:,:,channel].T)
+        img = Image.fromarray(surf.astype('uint8'),'L')
+        img_io_x = StringIO.StringIO()
+        img.save(img_io_x, 'jpeg', quality=40)
+        img_io_x.seek(0)
+        
+        frames['frame_'+str(frame_number)] = {
+            'z':base64.b64encode(img_io_z.read()),
+            'y':base64.b64encode(img_io_y.read()),
+            'x':base64.b64encode(img_io_x.read())
+        }
+
+    return jsonify(**frames)
+
+@app.route('/getFolders/<directory>')
+def getFolders(directory):
+    directory = directory.replace(':!','/')
+    subfolders = [os.path.basename(fname) for fname in
+        glob.glob(os.path.join(directory,'*')) if os.path.isdir(fname) or os.path.splitext(fname)[-1] =='.h5']
+    subfolders = ['']+subfolders
+    return render_template('select_list.html',options=subfolders) 
+
+@app.route('/saveImage', methods=['GET','POST'])
+def saveImage():
+    image = request.form.get('image')
+    filename = request.form.get('filename')
+    fh = open("/home/jack/movie/"+filename, "wb")
+    fh.write(image.decode('base64'))
+    fh.close()
+    return jsonify(status='complete')
